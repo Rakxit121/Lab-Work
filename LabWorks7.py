f37cadbd1242b6c27f758087f8fc36c336dc708f
Rakxit121 <rakxit121@outlook.com>
2021-12-01 22:54:33 +0545

    lab7

---
+++ LabWorks7.py
@@ -0,0 +1,7 @@
+# 7) Add odd numbers of first 10 integer
+ten = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+total = 0
+for i in range(1, 10):
+    if ten[i] % 2 != 0:
+        total = total + ten[i]
+print("The sum of even number is: ", total)
