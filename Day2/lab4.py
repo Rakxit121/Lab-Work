a23aa8fe62c29c304f794cdc087ad278ce67c57f
Rakxit121 <rakxit121@outlook.com>
2021-12-04 22:49:14 +0545

    apples

--- Day2/lab4.py
+++ Day2/lab4.py
@@ -1,19 +1,10 @@
-hour_first = int(input("Enter first hour:"))
-minute_first = int(input("Enter first minute:"))
+n = int(input("The number of students: "))
+k = int(input("The number of apples taken from the basket: "))
+# how many apples each student got
+d = k // n
+print(" The number of apple divide to tech student is: ", int(d))
 
-hour_second = int(input("Enter second hour:"))
-minute_second = int(input("Enter second minute:"))
+t = int(input("To find out the apples remaining apples in the basket is: "))
 
-diff_hour = 0
-diff_min = 0
-if hour_first > hour_second:
-    if minute_first > minute_second:
-        diff_min = minute_first - minute_second
-        diff_hour = hour_first - hour_second
-    else:
-        hour_first = hour_first - 1
-        minute_first = minute_first + 60
-        diff_min = minute_first - minute_second
-        diff_hour = hour_first - hour_second
-
-print(" The difference is ", diff_hour, " hour and", diff_min, " minute")
+r = t - k
+print("The remaining apples are: ", int(r))
