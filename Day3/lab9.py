8cab9faa080e19c33a9f7818ee287d4fba415afb
Rakxit121 <rakxit121@outlook.com>
2021-12-05 18:37:16 +0545

    lab9

--- Day3/lab9.py
+++ Day3/lab9.py
@@ -1,17 +1,30 @@
-u_name="random"
-passwd="pass"
+u_name = "random"
+passwd = "pass"
+j = 1
 
-
-for i= 1  3:
+for i in range(1):
     e_u_name = input("Enter the username: ")
     e_passwd = input("Enter the password: ")
-    if e_u_name==u_name and e_passwd== passwd:
+    j = j + 1
+    if e_u_name == u_name and e_passwd == passwd:
         print(" Logged in Successfully ")
         break
 else:
-    print("Try again!!")
-    e_u_name=input("")
-    e_passwd=input("")
-    continue
 
-
+    print("Error !!! Try again !!!")
+    e_u_name = input("Username: ")
+    e_passwd = input("Password: ")
+    j = j + 1
+    for i in range(2):
+        if e_u_name == u_name or e_passwd == passwd:
+            print(" Logged in Successfully ")
+            break
+        elif j > 3:
+            print("You have tried more than 3 times and cannot try again")
+            break
+        else:
+            print("Error !!! Try again !!!")
+            e_u_name = input("Username: ")
+            e_passwd = input("Password: ")
+            j = j + 1
+            continue
