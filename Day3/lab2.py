6eb171c7210b10dcb19268cfb8dafcb9d68787bf
Rakxit121 <rakxit121@outlook.com>
2021-12-06 15:43:45 +0545

    2nd

--- Day3/lab2.py
+++ Day3/lab2.py
@@ -2,11 +2,14 @@ marks_maths=int(input("Enter tha marks of  :"))
 marks_science=int(input("Enter tha marks of  :"))
 marks_computer=int(input("Enter tha marks of  :"))
 marks_english=int(input("Enter tha marks of  :"))
-Total_marks=100
-
-p_m=(marks_maths/Total_marks)*100
-p_s=(marks_science/Total_marks)*100
-p_c=(marks_computer/Total_marks)*100
-p_e=(marks_english/Total_marks)*100
-
-if 
+Total_scored=marks_maths+marks_science+marks_computer+marks_english
+print("The total marks scored in percentage is: ")
+per=(Total_scored/400)*100
+if per >70 and per <=100:
+    print("Distinction")
+elif per>60 and per<=70:
+    print("1st Division")
+elif per > 40 and per <=60:
+    print("2nd Division")
+else:
+    print("Fail")
