a71e428ad744b67350cb99e61c4598a55ecac63e
Rakxit121 <rakxit121@outlook.com>
2021-12-16 09:29:11 +0545

    Day3

--- Day6/List/list6.py
+++ Day6/List/list6.py
@@ -1,1 +1,6 @@
-sample = ['abc', 'xyz', 'aba', '1221', 'appllppa', 'apdjlrn']
+an_object = ['abc', 'xyz', 'aba', '1221', 'appllppa', 'apdjlrn']
+i = len(an_object)
+if i > 0:
+    print("The list is not empty")
+else:
+    print("The list is empty")
