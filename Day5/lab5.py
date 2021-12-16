4465fe40b07a039cd53349ba90114f3faf4977b0
Rakxit121 <rakxit121@outlook.com>
2021-12-16 08:17:19 +0545

    Day3

--- Day5/lab5.py
+++ Day5/lab5.py
@@ -1,6 +1,13 @@
 # reverse word
-word = input("Enter a word to reverse : ")
-a = ""
 
-for word in str:
+
+word = input("Enter a word to reverse : ")
+print("Input word: ",word)
+
+a = ""
+for ch in word:
     a = word + a
+
+print()
+print("Reversed word: ",a)
+
